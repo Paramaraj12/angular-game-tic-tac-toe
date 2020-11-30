1a317729c4ae5058c7ff63b6f5f41408f306d363
Paramaraj12 <paramaraj12@gmail.com>
2020-11-30 11:22:28 +0530

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { IconComponent } from "../icon/icon.component";
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [AppComponent,IconComponent],
-  bootstrap: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, IconComponent],
+  providers:[],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
