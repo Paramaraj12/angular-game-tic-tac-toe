8d5f296d82143a0b388f41824f08f5deebeae542
Paramaraj12 <paramaraj12@gmail.com>
2020-11-30 12:40:50 +0530

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { IconComponent } from "../icon/icon.component";
+//font-awesome
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome@0.6.0"
+
 @NgModule({
   imports: [BrowserModule, AppRoutingModule],
-  declarations: [AppComponent, IconComponent],
-  providers:[],
+  declarations: [AppComponent, IconComponent, FontAwesomeModule],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
