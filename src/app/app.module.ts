c773f16e9fbfa8d2f85ec524581f227803d7c434
Paramaraj12 <paramaraj12@gmail.com>
2020-11-30 14:18:17 +0530

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { IconComponent } from "../icon/icon.component";
+
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 //font-awesome
 
+//toaster
+
+import { ToastrModule } from "ngx-toastr";
 @NgModule({
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
+  ],
   declarations: [AppComponent, IconComponent],
   providers: [],
   bootstrap: [AppComponent]
--- src/icon/icon.component.ts
+++ src/icon/icon.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from "@angular/core";
-
+import { Component, Input } from "@angular/core";
+import { faPen, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faCircle } from "@fortawesome/free-regular-svg-icons";
 @Component({
   selector: "app-icon",
   templateUrl: "./icon.component.html",
   styleUrls: ["./icon.component.css"]
 })
-export class IconComponent implements OnInit {
-  constructor() {}
+export class IconComponent {
+  @Input() iconName: string;
 
-  ngOnInit() {}
+  faPen = faPen;
+  faTimes = faTimes;
+  faCircle = faCircle;
 }
